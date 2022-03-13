2c8c9f21b1c8d6373f1651f6e2555057c88967dd
Alex Kalashnikov <nixomsk@gmail.com>
2022-03-13 16:48:53 +0600

    Init

--- etc/restic/run.sh
+++ etc/restic/run.sh
@@ -5,14 +5,14 @@ cd $RESTIC_PATH
 echo Backup files
 bash ./backup-files.sh
 
-#echo Backup Postgres
-#bash ./backup-postgres.sh
+echo Backup Postgres
+bash ./backup-postgres.sh
 
-#echo Backup MySQL
-#bash ./backup-mariadb.sh
+echo Backup MySQL
+bash ./backup-mariadb.sh
 
 
 cd $RESTIC_PATH
 
-#echo Cleanup
-#restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12
+echo Cleanup
+restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12
